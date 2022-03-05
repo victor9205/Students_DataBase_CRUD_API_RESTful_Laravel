d167a407bb9ccef951f3a72107a0ec8d01a84cd4
victor9205 <victorrezende9205@gmail.com>
2022-03-05 18:36:27 -0300

    Second commit with initial project

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+
+Route::get('students', [ApiController::class, 'getAllStudents']);
+Route::get('students/{id}', [ApiController::class, 'getStudent']);
+Route::post('students', [ApiController::class, 'createStudent']);
+Route::put('students/{id}', [ApiController::class, 'updateStudent']);
+Route::delete('students/{id}', [ApiController::class, 'deleteStudent']);
+
